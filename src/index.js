da5ad9332896f74463308e8b066024c5b9824fd0
yuang <1157962772@qq.com>
2018-03-31 15:18:16 +0800

    跑起项目

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from '@/component/App';
+import App from './component/App';
 import registerServiceWorker from './registerServiceWorker';
 //import { BrowserRouter as Router, Route,Switch } from 'react-router-dom';
 //import Detail from '@/component/detail.js';
